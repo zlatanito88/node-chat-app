c617d3a8bacdd396d3866ae935224e7da33de41c
Vito Mastromarino <zlatanito88@gmail.com>
2018-03-04 11:41:34 +0100

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,24 @@
 var socket = io();
 
+function scrollToBottom () {
+
+    // selectors
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    // heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+
+};
+
 socket.on('connect', function () {
     console.log('Connected to the server');
 });
@@ -18,6 +37,7 @@ socket.on('newMessage', function(message) {
     });
 
     $("#messages").append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -30,6 +50,7 @@ socket.on('newLocationMessage', function(message) {
     });
 
     $("#messages").append(html);
+    scrollToBottom();
 });
 
 
